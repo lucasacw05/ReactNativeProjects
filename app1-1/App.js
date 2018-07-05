b55e740258f94a9c9aae9aae72c8e400b0a9ffbd
Lucas Alencar <lucasacw@gmail.com>
2018-07-05 17:35:30 -0300

    App1-1 Created

--- app1-1/App.js
+++ app1-1/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+
+let gerarNumeroAleatorio = () => {
+  alert("Teste")
+}
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          title= "Gerar número randômico."
+          onPress= {gerarNumeroAleatorio}
+        />
       </View>
     );
   }
@@ -18,6 +24,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 });
